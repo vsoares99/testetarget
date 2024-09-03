216a9a30c9f14c6ce57a3a485259f537e33d39e5
vsoares99 <victor.o.soares@hotmail.com>
2024-09-03 12:16:50 -0300

    Teste 02 Resolvido

---
+++ Teste02/Teste02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva uma palavra ou frase:");
+            string texto = Console.ReadLine();
+
+            int cont = 0;
+            foreach (char a in texto)
+            {
+                if (a == 'a' || a == 'A')
+                {
+                    cont++;
+                }
+            }
+
+            if (cont == 0)
+            {
+                Console.WriteLine("Letra 'a' não foi encontrada na string.");
+            }
+            else
+            {
+                Console.WriteLine($"Letra 'a' foi encontrada {cont} vez(es) na string.");
+            }
+        }
+    }
+}
